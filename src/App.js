aece300c3a0ffe6d6e8330371e013d849ba60b8f
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-03-18 13:45:51 +0530

    Setting up the Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "./Navbar";
+import Home from "./Home";
+//This is Type of Functional Component We can use in Our Wevsute
 function App() {
+  //we can define many const in the component and use in the return()
+  // we can define the link and use it in the anchor tag
+  //const person = {name:'Arijeet', age:30};
+  //booleans and object are not valid
+  //{title}(Dynamic Values)should be used to get the value of the title from the const
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <div className="content">
+      <Home/>
+      </div>
     </div>
   );
 }
