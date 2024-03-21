d69ff043e39b6e3c25cbe8e122e540bf4999e163
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-03-21 10:54:09 +0530

    Complete Final Version with DELETE req in API

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from "./Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Create from "./Create";
 import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
 //This is Type of Functional Component We can use in Our Wevsute
 function App() {
   //we can define many const in the component and use in the return()
@@ -24,6 +25,9 @@ function App() {
             </Route>
             <Route path="/blogs/:id">
               <BlogDetails />
+            </Route>
+            <Route path="*">
+              <NotFound />
             </Route>
           </Switch>
         </div>
